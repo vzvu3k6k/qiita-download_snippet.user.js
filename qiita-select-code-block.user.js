66f7ebccbfd8295cc3e15fd0bd5540701050da04
vzvu3k6k <vzvu3k6k@gmail.com>
2013-09-04 18:13:14 +0900

    Version Bump, 右上にボタンを追加する方式に変更
    menu要素でコンテキストメニューを操作できるのはFirefoxだけだった。

--- qiita-select-code-block.user.js
+++ qiita-select-code-block.user.js
@@ -1,34 +1,52 @@
 // ==UserScript==
 // @name           Qiita: Select code block
-// @description    コード部分のコンテキストメニューに「コードを選択」を追加
-// @version        1.0
+// @description    コードの右上のアイコンをクリックすると選択される
+// @version        1.1
 // @match          http://qiita.com/*
 // @namespace      http://qiita.com/vzvu3k6k/
 // @license        Public Domain
 // ==/UserScript==
 
-location.href = "javascript:(" + function(){
-    $(function(){
-        var elmActiveCodeFrame; /* .code-frame */
+var style = document.createElement("style");
+style.textContent = 
+    '.code-frame .__select-code {' +
+    '   display: none;' +
+    '}' +
+    '.code-frame:hover .__select-code {' +
+    '   display: block;' +
+    '   background: none repeat scroll 0 0 #EEEEEE;' +
+    '   border-bottom: 1px solid #CCCCCC;' +
+    '   border-left: 1px solid #CCCCCC;' +
+    '   border-radius: 0 3px 0 3px;' +
+    '   cursor: pointer;' +
+    '   padding: 3px;' +
+    '   float: right;' +
+    '}';
+document.head.appendChild(style);
 
-        var menu = $('<menu type="context" id="__selectcode">')
-            .append($('<menuitem label="コードを選択" id="__do-selectcode" />')
-                    .on("click", function(){
-                        select(elmActiveCodeFrame.querySelector("pre"));
-                    }));
+document.addEventListener("DOMNodeInserted", function(e){
+    var node = e.target;
+    if(node.nodeType == Node.ELEMENT_NODE && node.classList.contains("code-frame"))
+        if(!node.querySelector(".__select-code"))
+            addSelectButton(e.target);
+});
 
-        $("body").on("contextmenu", ".code-frame", function(e){
-            var ct = e.currentTarget;
-            elmActiveCodeFrame = ct;
-            $(ct).attr("contextmenu", "__selectcode");
-        }).append(menu);
+Array.prototype.forEach.call(document.querySelectorAll(".code-frame"), addSelectButton);
 
-        function select(elm){
-            var select = window.getSelection();
-            select.removeAllRanges();
-            var range = document.createRange();
-            range.selectNodeContents(elm);
-            select.addRange(range);
-        }
+function addSelectButton(elmCodeFrame){
+    var elmSelectButton = document.createElement("div");
+    elmSelectButton.setAttribute("class", "__select-code");
+    elmSelectButton.addEventListener("click", function(){
+        select(elmCodeFrame.querySelector("pre"));
     });
-} + ")()";
+    elmSelectButton.insertAdjacentHTML("afterbegin", '<i class="icon-paper-clip"/>');
+    elmCodeFrame.insertBefore(elmSelectButton, elmCodeFrame.firstChild);
+}
+
+function select(elm){
+    var select = window.getSelection();
+    select.removeAllRanges();
+    var range = document.createRange();
+    range.selectNodeContents(elm);
+    select.addRange(range);
+}
